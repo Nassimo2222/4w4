b3e53a60340004d0232920f7b54dcdf1e05a6971
Nassim-O Gagnon <simogagnon@gmail.com>
2022-03-11 15:30:23 -0500

    s7-c2 Nassim creation de sidebar pour le footer - clip-path pour la page d'accueil - custom logo pour l'entente

--- footer.php
+++ footer.php
@@ -1,24 +1,42 @@
 <footer class="site__footer">
-    <h2 class="footer__titre">Le MAGNIFIQUE footer!</h2>
-    <p class="footer__presentation">Un site web fait par un élève du TIM à l'aide de Wordpress.</p>
-    <p class="footer__presentation">Fait pour l'exercice 2.</p>
-    <h3 class="footer__author">Fait par Nassim-Olivier Gagnon</h3>
-    
-    
-
-<?php 
-
-$icone = '<svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#f00"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>';
-wp_nav_menu(array(
-                    "menu"=>"simple",
-                    "container"=>"nav",
-                    "container_class"=>"site__footer__menu",
-                    "menu_class"=>"site__footer__menu__ul",
-
-                    "link_before"=>$icone)); ?>
-                    <div class="footer__recherche"></div>
-                    <?php get_search_form(); ?>
+    <div class="site__footer__colonne">
+        <section class="footer__article">
+        <?php get_sidebar("footer_colonne_1"); ?>
+        </section>
+        <section class="footer__lien">
+        <?php get_sidebar("footer_colonne_2"); ?>
+        </section>
+        <section class="footer__adresse">
+        <?php get_sidebar("footer_colonne_3"); ?>
+        </section>
+    </div>
+    <div class="site__footer__ligne">
+        <section class="footer__description">
+        <?php get_sidebar("footer_ligne_2"); ?>
+        </section>
+        <section class="footer__copyright">
+        <?php get_sidebar("footer_ligne_1"); ?>
+        </section>
+        <section class="footer__auteur">
+            <p>Auteur : Eddy Martin</p>
+        </section>
+        <section class="footer__menu">
+        <?php    
+        $icone = '<svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#f00"><path fill-rule="evenodd" d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path><path fill-rule="evenodd" d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>';
+         wp_nav_menu(array(
+                        "menu"=>"simple",
+                        "container"=>"nav",
+                        "container_class"=>"footer__menu__nav",
+                        "menu_class"=>"footer__menu__nav__ul",
+                        "link_before"=>$icone)); ?>
+                <div class="footer__recherche">
+        </section>
+        <section class="footer__recherche">
+            <?php get_search_form(); ?>
+        </section>
+    </div>
 </footer>
+
 </body>
 <?php wp_footer(); ?>
 </html>
--- header.php
+++ header.php
@@ -11,9 +11,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <?php wp_head() ?>
+    <?php show_admin_bar(true); ?>
 </head>
 <body  <?php body_class("site"); ?>>
 <header class="site__header">
+    <?php get_custom_logo(); ?>
     <h1 class="header__titre"><?php bloginfo('name'); ?>
         <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
             <?php bloginfo( 'name' ); ?></h1>
