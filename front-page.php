120e42b7b3addc990a49890af338f0a14f2d5b91
Nassim-O Gagnon <simogagnon@gmail.com>
2022-03-17 12:56:45 -0400

    s7-c2 Rajouté des icones pour les liens, plus des WIT animations pour les différents cours. Réparé le problème du burger (persque)

--- front-page.php
+++ front-page.php
@@ -2,14 +2,15 @@
 <main class="site__main">
 
 <section class="animation">
-    <div class="animation__bloc">1</div>
-    <div class="animation__bloc">2</div>
-    <div class="animation__bloc">3</div>
-    <div class="animation__bloc">4</div>
-    <div class="animation__bloc">5</div>
+    <div class="animation__bloc">H</div>
+    <div class="animation__bloc">E</div>
+    <div class="animation__bloc">L</div>
+    <div class="animation__bloc">L</div>
+    <div class="animation__bloc">O</div>
+    <div class="animation__bloc">!</div>
 </section>
 
-    <h1>--- Front-page.php ---</h1>
+    <h1>PAGE PRINCIPALE</h1>
     <?php if (have_posts()): while(have_posts()) : the_post(); ?>
         <?php the_title() ?>
         <?php the_content() ?>
