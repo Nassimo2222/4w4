859bfcab4ca8b5cd6c5296ed208bd3ac8bc48b0b
Nassim-O Gagnon <simogagnon@gmail.com>
2022-03-18 14:54:50 -0400

    s8-c2 commit14 Rajouté des nouveaux icones links plus des descriptions commes celles des couleurs.

--- header.php
+++ header.php
@@ -56,6 +56,9 @@
     <label for="chk-burger" id="burger">
         <svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#ddd"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
     </label>
-    <?php wp_nav_menu(array("menu"=>"principal",
-                            "container"=>"nav")); ?>
+    <?php 
+        $iconeNav = '<svg width="32px" height="32px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#4CC9F0"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z" clip-rule="evenodd"></path></svg>';
+        wp_nav_menu(array("menu"=>"principal",
+                            "container"=>"nav",
+                            "link_before" => $iconeNav)); ?>
 </section>
