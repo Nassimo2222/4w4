0840fe9d90d752abef8a2e80c35920651ec69928
Nassim-O Gagnon <simogagnon@gmail.com>
2022-04-12 12:42:18 -0400

    s11 c1 Rajouté un nouveau template dans acf, rajouté du css pour les menu dans l'accueil, et modifié le sql binouche

--- gabarit/content-cours.php
+++ gabarit/content-cours.php
@@ -5,14 +5,16 @@
 <article class="formation__cours <?php echo $categories[1]->slug; ?> ">
         <?php
         $titre = get_the_title();
-        $titreFiltreCours = substr($titre, 7, -6);
+        $titreFiltreCours = substr($titre, 4, -6);
         /*$nbHeures = substr($titre, -6);*/
         $nbHeures = get_field( "nombre_dheures");
-        $sigleCours = substr($titre, 0, 7);
+        $departement = get_field("departement");
+        $sigleCours = substr($titre, 0, 3);
         $descCours = get_the_excerpt();
         ?>
         <?php the_post_thumbnail("thumbnail"); ?>
         <div class="cours__texte">
+
             <h3 class="cours__titre"> 
                 <a href="<?php echo get_permalink(); ?>">
                     <?= $titreFiltreCours; ?>
@@ -21,5 +23,6 @@
         <div class="cours__nbre-heure"><?= $nbHeures; ?></div>
         <p class="cours__sigle"><?= $sigleCours; ?> </p>
         <p class="cours__desc"> <?= $descCours; ?></p>
+        <p class="cours__dep"><?php $departement; ?></p>
     </div>
 </article>
