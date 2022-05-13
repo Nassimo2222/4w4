97951672ab85ed7936176bb28954e05bc9d9a561
Nassim-O Gagnon <simogagnon@gmail.com>
2022-05-13 17:27:45 -0400

    s15-c2 Commit finale et remise du site, rajout du evenement.scss et vérifications finale. Je croise mes doigt pour que ca soit bon !

--- template-evenement.php
+++ template-evenement.php
@@ -2,25 +2,39 @@
 <?php get_header() ?>
 <main class="site__main">
 
-    <h1>--- template-evenement.php ---</h1>
+    <h1>Évenement</h1>
     <?php if (have_posts()): the_post(); ?>
     <?php the_post_thumbnail(); ?>
         <?php the_title() ?>
-        <?php 
-            $image = get_field('image');
-            if( !empty( $image ) ): ?>
-                <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
-            <?php endif; ?>
-        <h3>L'endroit
-        </h3>
-        <p><?php the_field('endroit'); ?></p>
-        <p>Date de l'évenement: <?php the_field('date'); ?></p>
-        <p>L'heure: <?php the_field('heure'); ?></p>
-        <p>L'heure: <?php the_field('heure'); ?></p>
-        <h3>Résumé de l'évenement</h3>
-        <p><?php the_field('resume'); ?></p>
-        <h3>Organisateur de l'évenement</h3>
-        <p><?php the_field('organisateur'); ?></p>
+        <div class="evn__img">
+            <?php 
+                $image = get_field('image');
+                if( !empty( $image ) ): ?>
+                    <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+                <?php endif; ?>
+                <section class="evn__content">
+            <div class="evn__loc">
+                <h4>L'endroit
+                </h4>
+                <div class="evn__txt">
+                <p><?php the_field('endroit'); ?></p>
+                <p>Date de l'évenement: <?php the_field('date'); ?></p>
+                <p>L'heure: <?php the_field('heure'); ?></p>
+                <p>L'heure: <?php the_field('heure'); ?></p>
+                </div>
+            </div>
+            <div class="evn__res">
+                <h4>Résumé de l'évenement</h4>
+                <p><?php the_field('resume'); ?></p>
+            </div>
+            <div class="evn__org">
+                <h4>Organisateur de l'évenement</h4>
+                <p><?php the_field('organisateur'); ?></p>
+            </div>
+        </section>
+        </div>
+        
+        
     <?php endif ?>
 </main>
 <?php get_footer() ?>
